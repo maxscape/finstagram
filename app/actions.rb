a75899764e7a2bbb7dc9f11ae9f2b345d921bac6
Max Patterson <maxpatterson28@gmail.com>
2017-11-21 17:24:42 +0000

    Ruby ERB, Have not looped

--- app/actions.rb
+++ app/actions.rb
@@ -1,14 +1,5 @@
 get '/' do 
-    username = "sharky_j"
-    avatar_url = "http://naserca.com/images/sharky_j.jpg"
-    photo_url = "http://naserca.com/images/shark.jpg"
-    time_ago_in_minutes =
-    like_count = 0
-    comment_count = 1
-    comments = [
-        "sharky_j: out for the long weekend"]
-
-    #if the time_ago_in_minutes is more than 60
+     #if the time_ago_in_minutes is more than 60
     if time_ago_in_minutes >= 60
         # return this string
         "#{time_ago_in_minutes / 60} more than an hour ago"
@@ -19,4 +10,60 @@ get '/' do
         #return this instead
         "#{time_ago_in_minutes} minutes ago"
     end
+    erb :index
 end
+
+def humanized_time_ago(time_ago_in_minutes)
+  if time_ago_in_minutes == 60
+    "#{time_ago_in_minutes / 60} hours ago"
+  else
+    "#{time_ago_in_minutes} minutess ago"
+  end
+end
+
+get '/' do
+  @post_shark = {
+    username: "sharky_j",
+    avatar_url: "http://naserca.com/images/sharky_j.jpg",
+    photo_url: "http://naserca.com/images/shark.jpg",
+    humanized_time_ago: humanized_time_ago(15),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+      username: "sharky_j",
+      text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
+    }]
+  }
+
+  @post_whale = {
+    username: "kirk_whalum",
+    avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
+    photo_url: "http://naserca.com/images/whale.jpg",
+    humanized_time_ago: humanized_time_ago(65),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+      username: "kirk_whalum",
+      text: "#weekendvibes"
+    }]
+  }
+
+  @post_marlin = {
+    username: "marlin_peppa",
+    avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
+    photo_url: "http://naserca.com/images/marlin.jpg",
+    humanized_time_ago: humanized_time_ago(190),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+      username: "marlin_peppa",
+      text: "lunchtime! ;)"
+    }]
+  }
+
+    
+  @posts = [@post_shark, @post_whale, @post_marlin].to_s
+    
+    erb (:index)
+end
+
